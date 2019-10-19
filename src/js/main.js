4568ccb1d5f4ff8820cb4c625fceb98da41c648c
Bram Willemse <contact@bramwillemse.nl>
2019-10-19 12:46:12 +0200

    Site header / Set up site wide header, with logo an navigation

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import lazyLoader from './components/lazy-loader'
 import scrollAnimator from './components/scroll-animator'
 
+document.addEventListener('touchstart', function() {}, false)
 lazyLoader()
 scrollAnimator()
